1a27294dde9adde452d0478f44babc51a4a15848
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-25 01:41:08 -0200

    Update Matricula.py

--- Lp2/Matricula.py
+++ Lp2/Matricula.py
@@ -2,28 +2,27 @@
 import datetime,time
 
 class Matricula(object):
-    def __inti__(self):
+    
+    def __init__(self):
         self._aluno = None
         self.disciplina = None
         self._data_matricula = None
-        self._data_confirmacao=None
+        self._data_confirmacao = None
         self._data_cancelamento = None
 
-    def Altera_aluno(self,Aluno):
-        self._aluno=Aluno
+    def altera_aluno(self, Aluno):
+        self._aluno = Aluno
         return True
-
        
-    def Altera_Disciplina(self,Disciplina):
+    def altera_Disciplina(self, Disciplina):
         self._disciplina = Disciplina
         return True
     
-   
-    def Data_canfirmacao(self):
+    def data_canfirmacao(self):
         self._data_confirmacao = datetime.datetime.now()
         return self._data_confirmacao
     def Data_cancelamento(self):
-        self._data_cancelamento=datetime.datetime.now()
+        self._data_cancelamento = datetime.datetime.now()
         return self._data_cancelamento
     
     
