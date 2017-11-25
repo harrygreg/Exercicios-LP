81d76f6b66cbd6823d8706c7a360c59857b8f39c
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-25 01:40:14 -0200

    Update Professor.py

--- Lp2/Professor.py
+++ Lp2/Professor.py
@@ -1,25 +1,28 @@
 from Disciplina import Disciplina
 from Pessoa import Pessoa
+
 class Professor(object):
-    def __init(self):
-        
+    
+    def __init__(self):
+    
         self._apelido = None
-        self._disciplina=None
+        self._disciplina = None
 
     def getApelido(self):
         return self._apelido
-    def setApelido(self,Apelido):
-        self._apelido=Apelido
+    
+    def setApelido(self, Apelido):
+        self._apelido = Apelido
        
-        
-    def adiciona_disciplina(self,lista,lista2,Nome,Carga,):
+    def adiciona_disciplina(self, lista, lista2, Nome, Carga,):
         if Nome in lista:
             return False 
         else:
             lista.append(Nome)
             lista2.append(Carga)
             return True
-    def deleta_disciplina(self,lista,lista2,Nome):
+        
+    def deleta_disciplina(self, lista, lista2, Nome):
         
         if Nome in lista:
             x=lista.index(Nome)
@@ -28,7 +31,8 @@ class Professor(object):
             return True
         else:
             return False
-    def total_carga_horaria(self,lista2):
+        
+    def total_carga_horaria(self, lista2):
         soma = sum(lista2)
         return soma
 
