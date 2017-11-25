1c6e311a570d97d6932012e41520e265ebc182c8
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-25 01:42:44 -0200

    Update Aluno.py

--- Lp2/Aluno.py
+++ Lp2/Aluno.py
@@ -1,23 +1,24 @@
 from Matricula import Matricula
 
 class Aluno(object):
-    def __init__(self):
-        
+    
+    def __init__(self):    
         self._sigla_curso = None
         self._matriculas = None
-    def Disciplinas_alunos(self,lista,Disciplina,Aluno):
+        
+    def disciplinas_alunos(self, lista, Disciplina, Aluno):
         lista.append(Disciplina)
         lista.append(Aluno)
         return lista
     
-    def getSigla(self,Sigla):
+    def getSigla(self, Sigla):
         self._sigla_curso = Sigla
         
-    def Matricular(self,Matricula,ListMatricula):
+    def matricular(self, Matricula, ListMatricula):
         ListMatricula.append(Matricula)
         return True
 
-    def confirmar_matricula(self,matricula,ListaMatricula,Disp,ListaDisp):
+    def confirmar_matricula(self, matricula, ListaMatricula, Disp, ListaDisp):
         if Disp in ListaMatricula:
             return False
         else:
@@ -26,11 +27,9 @@ class Aluno(object):
                 Matricula.Data_canfirmacao(self)
                 return Matricula.Data_canfirmacao(self)
         
-    def cancelar_matricula(self,matricula,ListaMatricula):
+    def cancelar_matricula(self, matricula, ListaMatricula):
         if matricula in ListMatricula:
-            
             Matricula.Data_cancelamento(self)
-            
             return Matricula.Data_cancelamento(self)
 
         
