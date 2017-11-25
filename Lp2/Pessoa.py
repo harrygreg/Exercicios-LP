07c1e5ca37d700b21bb195cff3e2cbdd71545a95
Douglas Garcia <douglas.garcia@ymail.com>
2017-11-25 01:38:00 -0200

    Update Pessoa.py

--- Lp2/Pessoa.py
+++ Lp2/Pessoa.py
@@ -1,27 +1,29 @@
 class Pessoa(object):
-    def __init(self):
-        self._nome=None
-        self._email=None
-        self._celular=None
+    
+    def __init__(self):
+        self._nome = None
+        self._email = None
+        self._celular = None
         
-    def Retorna_Nome(self):
+    def retorna_Nome(self):
         return self._nome
     
-    def Retorna_Email(self):
+    def retorna_Email(self):
         return self._email
     
-    def Retorna_Celular(self):
+    def retorna_Celular(self):
         return self._celular
-    
    
-    def alterar_Nome(self,Nome):
-        self._nome=Nome
+    def alterar_Nome(self, Nome):
+        self._nome = Nome
         return True
-    def alterar_Celular(self,Celular):
-        self._celular=Celular
+    
+    def alterar_Celular(self, Celular):
+        self._celular = Celular
         return True
-    def alterar_Email(self,Email):
-        self._email=Email
+    
+    def alterar_Email(self, Email):
+        self._email = Email
         return True
 
 
